2f6df5b6dd9c37562f28e5db25854da7f23fe969
Enmanuel Medina <enmanuelm19@gmail.com>
2016-09-14 17:34:04 -0430

    Fixing link recognition

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,6 @@ class Task < ActiveRecord::Base
 
   auto_html_for :content do
     html_escape
-    image
     youtube(width: '80%', height: 250 )
     link target: '_blank', rel: 'nofollow'
     simple_format
